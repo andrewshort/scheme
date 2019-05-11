1c443c85ffce798344d9ada64c829452cface642
ashort <andrews228@gmail.com>
2019-05-10 22:20:49 -0400

    debugging auth callback

--- routes/auth.js
+++ routes/auth.js
@@ -18,8 +18,11 @@ router.get('/login', passport.authenticate('auth0', {
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
+    console.log('passport.authenticate')
+    console.log(err);
+    console.log(user);
     if (err) { return next(err); }
-    if (!user) { return res.redirect('/login'); }
+    //if (!user) { return res.redirect('/login'); }
     req.logIn(user, function (err) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
